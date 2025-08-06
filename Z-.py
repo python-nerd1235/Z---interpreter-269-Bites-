4621228580884276494f6af74d7cfaaebb3c89da
python-nerd1235 <calboyg93@gmail.com>
2025-08-06 19:00:14 -0400

    Z-.py

--- Z-.py
+++ Z-.py
@@ -7,9 +7,10 @@ while P<len(T):
     elif T[P]=='-':
             C=C-1
     elif T[P]==')':
-        if C==0:
+        if C!=0:
             while T[P]!='(':
                 P=P-1
     elif T[P]=='$':
         print(chr(C))
+
     P+=1
