5a5e872392c5ced88cc669532fdad3e54b0a35d9
python-nerd1235 <calboyg93@gmail.com>
2025-08-06 18:50:00 -0400

    Add files via upload

---
+++ Z-.py
@@ -0,0 +1,15 @@
+T=list(input('')) 
+C=0
+P=0
+while P<len(T):
+    if T[P]=='+':
+            C=C+1
+    elif T[P]=='-':
+            C=C-1
+    elif T[P]==')':
+        if C==0:
+            while T[P]!='(':
+                P=P-1
+    elif T[P]=='$':
+        print(chr(C))
+    P+=1
